5212075877d3cd4f82ee2158414c6e7bd091c0ee
Diego <diegoteixeirapereira@hotmail.com>
2021-08-06 17:33:54 -0300

    alteração nos arquivos componentes app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosComponent } from './admin/usuarios/usuarios.component';
+import { LivrosComponent } from './admin/livros/livros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/usuarios', component: UsuariosComponent
+  },
+  {
+    path: 'admin/livros', component: LivrosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { UsersComponent } from './admin/users/users.component';
+import { UsuariosComponent } from './admin/usuarios/usuarios.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddUsuarioComponent } from './admin/usuarios/add-usuario/add-usuario.component';
+import { MostrarUsuarioComponent } from './admin/usuarios/mostrar-usuario/mostrar-usuario.component';
+import { LivrosComponent } from './admin/livros/livros.component';
+import { AddLivroComponent } from './admin/livros/add-livro/add-livro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UsersComponent,
+    UsuariosComponent,
+    AddUsuarioComponent,
+    MostrarUsuarioComponent,
+    LivrosComponent,
+    AddLivroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
